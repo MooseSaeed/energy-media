76d12a728bb6104d8a313e5aef0c414694cebe91
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-06-07 19:03:14 +0200

    Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="flex flex-col justify-center items-center sm:mt-8 mt-20">
+    <div class="max-w-72 order-1 md:-order-none">
+      <div class="max-w-xs sm:max-w-xl animate__animated animate__flipInX">
+        <Energy />
+      </div>
+    </div>
+    <section class="order-2 md:-order-none pb-2">
+      <header
+        class="pb-2 overflow-hidden flex flex-col justify-center items-center"
+      >
+        <h1
+          class="dark:text-white text-center text-white font-bold text-3xl leading-relaxed animate__animated animate__bounceInUp"
+        >
+          <div class="text-center">
+            <span class="text-2xl sm:text-3xl">For</span>
+            <span class="inline">
+              <keep-alive>
+                <Changingname />
+              </keep-alive>
+            </span>
+            <div class="text-2xl sm:text-3xl">
+              Media Production and Advertising
+            </div>
+          </div>
+        </h1>
+        <p
+          class="mt-5 text-lg text-center max-w-lg text-white animate__animated animate__bounceInUp animate__slow"
+        >
+          This is the right place for you if you want to take your brand to the
+          next level of success and popularity.
+        </p>
+      </header>
+      <footer>
+        <div
+          class="flex flex-col sm:flex-row gap-4 items-center justify-center overflow-hidden py-5 px-2"
+        >
+          <a
+            href="/tools"
+            class="animate__animated animate__bounceInUp animate__delay-1s"
+          >
+            <Primarybtn class="text-sm sm:text-base">
+              GET TO BUSINESS
+            </Primarybtn>
+          </a>
+          <a
+            class="ml-3 animate__animated animate__bounceInUp animate__slow animate__delay-1s"
+            href="/components"
+          >
+            <Secondarybtn class="text-sm sm:text-base"> ABOUT US </Secondarybtn>
+          </a>
+        </div>
+      </footer>
+    </section>
+  </div>
+</template>
+
+<script>
+import Primarybtn from "./Primarybtn.vue";
+import Secondarybtn from "./Secondarybtn.vue";
+import Changingname from "./Changingname.vue";
+import Energy from "./Energy.vue";
+export default {
+  name: "Homepage",
+  components: {
+    Primarybtn,
+    Secondarybtn,
+    Changingname,
+    Energy,
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,76 +1,15 @@
 <template>
   <main class="min-h-screen flex justify-center items-center">
-    <div class="flex flex-col justify-center items-center sm:mt-8 mt-20">
-      <div class="max-w-72 order-1 md:-order-none">
-        <div class="max-w-xs sm:max-w-xl animate__animated animate__flipInX">
-          <Energy />
-        </div>
-      </div>
-      <section class="order-2 md:-order-none pb-2">
-        <header
-          class="pb-2 overflow-hidden flex flex-col justify-center items-center"
-        >
-          <h1
-            class="dark:text-white text-center text-white font-bold text-3xl leading-relaxed animate__animated animate__bounceInUp"
-          >
-            <div class="text-center">
-              <span class="text-2xl sm:text-3xl">For</span>
-              <span class="inline">
-                <keep-alive>
-                  <Changingname />
-                </keep-alive>
-              </span>
-              <div class="text-2xl sm:text-3xl">
-                Media Production and Advertising
-              </div>
-            </div>
-          </h1>
-          <p
-            class="mt-5 text-lg text-center max-w-lg text-white animate__animated animate__bounceInUp animate__slow"
-          >
-            This is the right place for you if you want to take your brand to
-            the next level of success and popularity.
-          </p>
-        </header>
-        <footer>
-          <div
-            class="flex flex-col sm:flex-row gap-4 items-center justify-center overflow-hidden py-5 px-2"
-          >
-            <a
-              href="/tools"
-              class="animate__animated animate__bounceInUp animate__delay-1s"
-            >
-              <Primarybtn class="text-sm sm:text-base">
-                GET TO BUSINESS
-              </Primarybtn>
-            </a>
-            <a
-              class="ml-3 animate__animated animate__bounceInUp animate__slow animate__delay-1s"
-              href="/components"
-            >
-              <Secondarybtn class="text-sm sm:text-base">
-                ABOUT US
-              </Secondarybtn>
-            </a>
-          </div>
-        </footer>
-      </section>
-    </div>
+    <Hero />
   </main>
 </template>
 
 <script>
-import Primarybtn from "../components/Primarybtn.vue";
-import Secondarybtn from "../components/Secondarybtn.vue";
-import Changingname from "../components/Changingname.vue";
-import Energy from "../components/Energy.vue";
+import Hero from "../components/Hero.vue";
 export default {
   name: "Homepage",
   components: {
-    Primarybtn,
-    Secondarybtn,
-    Changingname,
-    Energy,
+    Hero,
   },
 };
 </script>
