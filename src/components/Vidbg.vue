de0abb4704cdc85964504bbf45083e4c5aa19b5d
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-06-07 19:38:28 +0200

    video (Go back point)

---
+++ src/components/Vidbg.vue
@@ -0,0 +1,19 @@
+<template>
+  <VideoBackground
+    :src="require(`@/assets/images/sample.mp4`)"
+    style="max-height: 400px; height: 100vh"
+  >
+    <h1 style="color: white">Hello welcome!</h1>
+  </VideoBackground>
+</template>
+
+<script>
+import VideoBackground from "vue-responsive-video-background-player";
+export default {
+  components: {
+    VideoBackground,
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
   <main class="min-h-screen flex justify-center items-center">
     <Hero />
+    <Vidbg />
   </main>
 </template>
 
 <script>
 import Hero from "../components/Hero.vue";
+import Vidbg from "../components/Vidbg.vue";
 export default {
   name: "Homepage",
   components: {
     Hero,
+    Vidbg,
   },
 };
 </script>
