f2e495f16562541a55d809af7839bd3209c4cdad
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-06-08 22:27:56 +0200

    hero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
   <main class="relative h-screen flex justify-center items-center">
-    <div class="z-10">
-      <Hero />
-    </div>
     <div class="absolute left-0 right top-0 h-full w-full z-0">
       <Vidbg />
+    </div>
+    <div class="z-10">
+      <Hero />
     </div>
   </main>
 </template>
