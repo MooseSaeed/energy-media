fc8e0034e108aefa31d6e55079b1baa9c0300932
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-06-19 19:20:56 +0200

    embedding vimeo

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -14,154 +14,34 @@
             <div
               class="h-full rounded-xl bg-gradient-to-r shadow-md shadow-black from-slate-800 to-gray-900 overflow-hidden"
             >
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
-                src="../assets/images/robotthumbnail.png"
-                alt="sample"
-              />
-              <div class="p-6">
-                <h1 class="title-font text-lg font-medium text-gray-200 mb-3">
-                  A Big Title Goes Here
-                </h1>
-
-                <p class="leading-relaxed mb-5 text-white">Some description</p>
-                <div class="flex items-center flex-wrap">
-                  <a href="/tools">
-                    <Primarybtn class="text-sm sm:text-base">
-                      Check Demo
-                    </Primarybtn>
-                  </a>
+              <div
+                class="lg:h-48 md:h-36 w-full object-cover object-center md:scale-105 transition-all duration-400 hover:scale-100"
+              >
+                <div style="padding: 56.25% 0 0 0; position: relative">
+                  <iframe
+                    src="https://player.vimeo.com/video/720190144?h=4ca72a65f2&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                    frameborder="0"
+                    allow="autoplay; fullscreen; picture-in-picture"
+                    allowfullscreen
+                    style="
+                      position: absolute;
+                      top: 0;
+                      left: 0;
+                      width: 100%;
+                      height: 100%;
+                    "
+                    title="Commercial video"
+                  ></iframe>
                 </div>
               </div>
-            </div>
-          </div>
-          <!-- Card -->
-          <div class="p-4 md:w-1/3">
-            <div
-              class="h-full rounded-xl bg-gradient-to-r shadow-md shadow-black from-slate-800 to-gray-900 overflow-hidden"
-            >
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
-                src="../assets/images/robotthumbnail.png"
-                alt="sample"
-              />
               <div class="p-6">
-                <h1 class="title-font text-lg font-medium text-gray-200 mb-3">
+                <h1
+                  class="title-font text-lg font-medium text-gray-200 mt-7 mb-3"
+                >
                   A Big Title Goes Here
                 </h1>
 
                 <p class="leading-relaxed mb-5 text-white">Some description</p>
-                <div class="flex items-center flex-wrap">
-                  <a href="/tools">
-                    <Primarybtn class="text-sm sm:text-base">
-                      Check Demo
-                    </Primarybtn>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- Card -->
-          <div class="p-4 md:w-1/3">
-            <div
-              class="h-full rounded-xl bg-gradient-to-r shadow-md shadow-black from-slate-800 to-gray-900 overflow-hidden"
-            >
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
-                src="../assets/images/robotthumbnail.png"
-                alt="sample"
-              />
-              <div class="p-6">
-                <h1 class="title-font text-lg font-medium text-gray-200 mb-3">
-                  A Big Title Goes Here
-                </h1>
-
-                <p class="leading-relaxed mb-5 text-white">Some description</p>
-                <div class="flex items-center flex-wrap">
-                  <a href="/tools">
-                    <Primarybtn class="text-sm sm:text-base">
-                      Check Demo
-                    </Primarybtn>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- Card -->
-          <div class="p-4 md:w-1/3">
-            <div
-              class="h-full rounded-xl bg-gradient-to-r shadow-md shadow-black from-slate-800 to-gray-900 overflow-hidden"
-            >
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
-                src="../assets/images/robotthumbnail.png"
-                alt="sample"
-              />
-              <div class="p-6">
-                <h1 class="title-font text-lg font-medium text-gray-200 mb-3">
-                  A Big Title Goes Here
-                </h1>
-
-                <p class="leading-relaxed mb-5 text-white">Some description</p>
-                <div class="flex items-center flex-wrap">
-                  <a href="/tools">
-                    <Primarybtn class="text-sm sm:text-base">
-                      Check Demo
-                    </Primarybtn>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- Card -->
-          <div class="p-4 md:w-1/3">
-            <div
-              class="h-full rounded-xl bg-gradient-to-r shadow-md shadow-black from-slate-800 to-gray-900 overflow-hidden"
-            >
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
-                src="../assets/images/robotthumbnail.png"
-                alt="sample"
-              />
-              <div class="p-6">
-                <h1 class="title-font text-lg font-medium text-gray-200 mb-3">
-                  A Big Title Goes Here
-                </h1>
-
-                <p class="leading-relaxed mb-5 text-white">Some description</p>
-                <div class="flex items-center flex-wrap">
-                  <a href="/tools">
-                    <Primarybtn class="text-sm sm:text-base">
-                      Check Demo
-                    </Primarybtn>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- Card -->
-          <div class="p-4 md:w-1/3">
-            <div
-              class="h-full rounded-xl bg-gradient-to-r shadow-md shadow-black from-slate-800 to-gray-900 overflow-hidden"
-            >
-              <img
-                class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
-                src="../assets/images/robotthumbnail.png"
-                alt="sample"
-              />
-              <div class="p-6">
-                <h1 class="title-font text-lg font-medium text-gray-200 mb-3">
-                  A Big Title Goes Here
-                </h1>
-
-                <p class="leading-relaxed mb-5 text-white">Some description</p>
-                <div class="flex items-center flex-wrap">
-                  <a href="/tools">
-                    <Primarybtn class="text-sm sm:text-base">
-                      Check Demo
-                    </Primarybtn>
-                  </a>
-                </div>
               </div>
             </div>
           </div>
